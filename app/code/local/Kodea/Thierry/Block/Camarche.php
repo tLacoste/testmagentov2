ac5a18a75f9af4bfe5b7b0603121848229332021
thierry <thierry@kodea.fr>
2017-06-01 18:06:52 +0200

    tes modifications sur le rewrite model

--- app/code/local/Kodea/Thierry/Block/Camarche.php
+++ app/code/local/Kodea/Thierry/Block/Camarche.php
@@ -5,7 +5,7 @@ class Kodea_Thierry_Block_Camarche extends Mage_Core_Block_Template
      {
          $monClient=new Mage_Customer_Model_Customer();
 //         $monClient=new Kodea_Rewrite_Model_Customer();
-         return "Ca marche<br/>Mon model modifié est de classe: ".$monClient->getName().get_class($monClient);
+         return "Ca marche<br/><br/>Mon model modifié devrait être de classe<br/>'Kodea_Rewrite_Model_Customer' et est de classe: <br/>'".get_class($monClient)."'<br/>".$monClient->getName();
      }
      
      public function rewriteblock()
